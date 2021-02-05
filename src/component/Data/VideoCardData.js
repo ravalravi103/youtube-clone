fa950981690cd028261c4e561e6173fa92d6e757
ravalravi103 <ravalravi103@gmail.com>
2021-02-05 12:35:58 +0530

    videocard cleaning work done

---
+++ src/component/Data/VideoCardData.js
@@ -0,0 +1,59 @@
+
+export const videoData = [
+    {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   },
+   {
+      imageSrc: 'https://i.ytimg.com/an_webp/FL4z2x3vdSk/mqdefault_6s.webp?du=3000&sqp=CI-084AG&rs=AOn4CLDdNQYmQdquQTTUZjPLZH71dE_wDQ',
+      videoTitle: 'Taarak Mehta Ka Ooltah Chashmah - Ep 2684 - Full Episode - 11th March, 2019',
+      chanelIcon:'https://yt3.ggpht.com/ytc/AAUvwnjEgTjK9JfqjP5zO-S8r10bbMnA6H5ESijhvJKZSHU=s88-c-k-c0x00ffffff-no-rj',
+      channelName:'xplosiveWebTech',
+      viewsAndTime:"1.2M  11hr ago"
+   }
+]
